6a5ee75bcbe3f25b852ca4d516639dbb2500d66e
oswaldo jose anzola gutierrez <rapanuti@gmail.com>
2024-08-03 10:46:55 -0400

    Update venezuela.sql
    Actualización del estado Vargas a La Guaira

--- venezuela.sql
+++ venezuela.sql
@@ -65,7 +65,7 @@ CREATE TABLE `estados` (
 
 LOCK TABLES `estados` WRITE;
 /*!40000 ALTER TABLE `estados` DISABLE KEYS */;
-INSERT INTO `estados` VALUES (1,'Amazonas'),(2,'Anzoátegui'),(3,'Apure'),(4,'Aragua'),(5,'Barinas'),(6,'Bolívar'),(7,'Carabobo'),(8,'Cojedes'),(9,'Delta Amacuro'),(10,'Falcón'),(11,'Guárico'),(12,'Lara'),(13,'Mérida'),(14,'Miranda'),(15,'Monagas'),(16,'Nueva Esparta'),(17,'Portuguesa'),(18,'Sucre'),(19,'Táchira'),(20,'Trujillo'),(21,'Vargas'),(22,'Yaracuy'),(23,'Zulia'),(24,'Distrito Capital'),(25,'Dependencias Federales');
+INSERT INTO `estados` VALUES (1,'Amazonas'),(2,'Anzoátegui'),(3,'Apure'),(4,'Aragua'),(5,'Barinas'),(6,'Bolívar'),(7,'Carabobo'),(8,'Cojedes'),(9,'Delta Amacuro'),(10,'Falcón'),(11,'Guárico'),(12,'Lara'),(13,'Mérida'),(14,'Miranda'),(15,'Monagas'),(16,'Nueva Esparta'),(17,'Portuguesa'),(18,'Sucre'),(19,'Táchira'),(20,'Trujillo'),(21,'La Guaira'),(22,'Yaracuy'),(23,'Zulia'),(24,'Distrito Capital'),(25,'Dependencias Federales');
 /*!40000 ALTER TABLE `estados` ENABLE KEYS */;
 UNLOCK TABLES;
 
